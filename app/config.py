b7e348d5ecb70588be2257ff5d20ecf2182aac4d
noburing64 <higher244.shassu77@gmail.com>
2022-08-18 10:50:02 +0900

    fix: .env修正

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ dotenv_path=join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 SECRET_KEY=os.environ.get("SECRET_KEY", "secret")
-API_DOMAIN=os.environ.get("API_BASE_URL", "api:8080")
+API_DOMAIN=os.environ.get("API_DOMAIN", "api:8080")
 AWS_S3_ENDPOINT_URL=os.environ.get("AWS_S3_ENDPOINT_URL", "localhost")
 AWS_S3_REGION_NAME=os.environ.get("S3_REGION_NAME", "")
 AWS_ACCESS_KEY_ID=os.environ.get("AWS_ACCESS_KEY_ID", "")
