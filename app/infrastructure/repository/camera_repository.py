1901175356448fffc0d4b956ad2ad9aa1bfd2097
noburing64 <higher244.shassu77@gmail.com>
2022-08-18 15:10:24 +0900

    update: https対応

--- app/infrastructure/repository/camera_repository.py
+++ app/infrastructure/repository/camera_repository.py
@@ -1,34 +1,16 @@
 from domain.repository.camera_repository import AbsCameraRepository
-import http.client
-import json
+import domain.helper.http_helper as http_helper
 import config
 from urllib.parse import urlencode
 
 class CameraRepository(AbsCameraRepository):
     def FindAll(self):
-        connection = http.client.HTTPConnection(config.API_DOMAIN)
-        connection.request("GET", "/api/v1/cameras")
-        response = connection.getresponse().read()
-        data = json.loads(response)
-        connection.close()
-        return data
+        return http_helper.request(config.API_DOMAIN, "/api/v1/cameras", "GET")
         
     def FindByModelName(self, model_name):
-        connection = http.client.HTTPConnection(config.API_DOMAIN)
-        header = {'content-type': 'application/json'}
         url = f"/api/v1/cameras?{urlencode({'model_name': model_name})}"
-        connection.request("GET", url, headers=header)
-        response = connection.getresponse().read()
-        data = json.loads(response)
-        connection.close()
-        return data
+        return http_helper.request(config.API_DOMAIN, url, "GET")
         
     def Create(self, model_name, focal_length):
-        connection = http.client.HTTPConnection(config.API_DOMAIN)
-        header = {'content-type': 'application/json'}
         post_data = {"model_name": model_name, "focal_length": focal_length}
-        connection.request("POST", "/api/v1/cameras", body=json.dumps(post_data), headers=header)
-        response = connection.getresponse().read()
-        data = json.loads(response)
-        connection.close()
-        return data
+        return http_helper.request(config.API_DOMAIN, "/api/v1/cameras", "POST", post_data)
--- app/infrastructure/repository/object_model_repository.py
+++ app/infrastructure/repository/object_model_repository.py
@@ -1,21 +1,20 @@
 from domain.repository.object_model_repository import AbsObjectModelRepository
-import http.client
-import json
+import domain.helper.http_helper as http_helper
 import config
 
 class ObjectModelRepository(AbsObjectModelRepository):        
     def FindByHash(self, hash: str):
         url = f"/api/v1/object_models/{hash}"
-        return self.__request(config.API_DOMAIN, url, "GET")
+        return http_helper.request(config.API_DOMAIN, url, "GET")
         
     def UpdatePid(self, object_model_id, pid):        
         url = f"/api/v1/object_models/{object_model_id}/pid"
-        return self.__request(config.API_DOMAIN, url, "PATCH", {"pid": pid})
+        return http_helper.request(config.API_DOMAIN, url, "PATCH", {"pid": pid})
         
     def CreateProcessHistory(self, object_model_id, process_type_id):
         upload_data = {"process_type_id": process_type_id}
         url = f"/api/v1/object_models/{object_model_id}/process_histories"
-        return self.__request(config.API_DOMAIN, url, "POST", upload_data)
+        return http_helper.request(config.API_DOMAIN, url, "POST", upload_data)
         
     def CreateModelObjectFile(self, object_model_id, file_path, file_name, file_size):
         upload_data = {
@@ -24,24 +23,5 @@ class ObjectModelRepository(AbsObjectModelRepository):
             "file_size": file_size
         }
         url = f"/api/v1/object_models/{object_model_id}/object_model_files"
-        return self.__request(config.API_DOMAIN, url, "POST", upload_data)
-
-    def __request(self, domain: str, path: str, method: str, request_data = None) -> dict:
-        conn = http.client.HTTPConnection(domain)
-        headers = {'content-type': 'application/json'}
-        body = json.dumps(request_data) if request_data is dict else None
-        
-        conn.request(method.upper(), path, headers=headers, body=body)
-        response = conn.getresponse()
-        if response.status == 302:
-            # HTTPSにリダイレクト
-            conn = http.client.HTTPSConnection(domain)
-            conn.request(method.upper(), path, headers=headers, body=body)
-            response = conn.getresponse()
-            data = json.loads(response.read())
-        else:
-            data = json.loads(response.read())
-        
-        conn.close()
-        return data
+        return http_helper.request(config.API_DOMAIN, url, "POST", upload_data)
     
