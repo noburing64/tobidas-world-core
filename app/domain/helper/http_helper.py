a65997e0b1bb7f819e8a0bb5c9b80a6a95902ab1
noburing64 <higher244.shassu77@gmail.com>
2022-08-22 20:49:18 +0900

    fix: POST等ができない問題を修正

--- app/domain/helper/http_helper.py
+++ app/domain/helper/http_helper.py
@@ -4,7 +4,7 @@ import json
 def request(domain: str, path: str, method: str, request_data = None) -> dict:
     conn = http.client.HTTPConnection(domain)
     headers = {'content-type': 'application/json'}
-    body = json.dumps(request_data) if request_data is dict else None
+    body = json.dumps(request_data) if type(request_data) is dict else None
     
     conn.request(method.upper(), path, headers=headers, body=body)
     response = conn.getresponse()
